82f83861e454c6a83b75f8f33ba4ceaaaa91cf29
FourSqweez <four7518@gmail.com>
2021-05-26 00:35:05 +0700

    re deploy

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,62 +4,70 @@ import Section from './Section'
 import { Element } from 'react-scroll'
 
 export default function Home() {
-	const [infos, setInfos] = useState([])
-	const [fetchDone, setFetchDone] = useState(false)
-	const getData = async () => {
-		const results = await fetch('carinfo.json').then((res) => res.json())
-		setInfos(results)
-		setFetchDone(true)
-	}
-
-	useEffect(() => {
-		getData()
-	}, [])
 	return (
 		<Container>
-			{infos &&
-				infos.map((info) => (
-					<Element id={`section${info.id}`} key={info.id}>
-						<Section
-							key={info.id}
-							title={info.title}
-							description={info.description}
-							backgroundImg={info.image}
-							leftBtnText="Custom order"
-							rightBtnText="Existing inventory"
-						/>
-					</Element>
-				))}
+			<Element id="section0">
+				<Section
+					title="Model S"
+					description="Order Online for Touchless Delivery"
+					backgroundImg="model-s.jpg"
+					leftBtnText="Custom order"
+					rightBtnText="Existing inventory"
+				/>
+			</Element>
+			<Element id="section1">
+				<Section
+					title="Model 3"
+					description="Order Online for Touchless Delivery"
+					backgroundImg="model-3.jpg"
+					leftBtnText="Custom order"
+					rightBtnText="Existing inventory"
+				/>
+			</Element>
+			<Element id="section2">
+				<Section
+					title="Model X"
+					description="Order Online for Touchless Delivery"
+					backgroundImg="model-x.jpg"
+					leftBtnText="Custom order"
+					rightBtnText="Existing inventory"
+				/>
+			</Element>
+			<Element id="section3">
+				<Section
+					title="Model Y"
+					description="Order Online for Touchless Delivery"
+					backgroundImg="model-y.jpg"
+					leftBtnText="Custom order"
+					rightBtnText="Existing inventory"
+				/>
+			</Element>
 
-			{fetchDone && (
-				<>
-					<Element id="solar-roof">
-						<Section
-							title="Solar for New Roofs"
-							description="Solar Roofs Costs Less Than a New Roof Plus Solar Panels"
-							backgroundImg="solar-roof.jpg"
-							leftBtnText="Order now"
-							rightBtnText="Learn more"
-						/>
-					</Element>
-					<Element id="solar-panels">
-						<Section
-							title="Lowest Cost Solar Panels in America"
-							description="Money-back guarantee"
-							backgroundImg="solar-panel.jpg"
-							leftBtnText="Order now"
-							rightBtnText="Learn more"
-						/>
-					</Element>
+			<Element id="solar-roof">
+				<Section
+					title="Solar for New Roofs"
+					description="Solar Roofs Costs Less Than a New Roof Plus Solar Panels"
+					backgroundImg="solar-roof.jpg"
+					leftBtnText="Order now"
+					rightBtnText="Learn more"
+				/>
+			</Element>
+			<Element id="solar-panels">
+				<Section
+					title="Lowest Cost Solar Panels in America"
+					description="Money-back guarantee"
+					backgroundImg="solar-panel.jpg"
+					leftBtnText="Order now"
+					rightBtnText="Learn more"
+				/>
+			</Element>
 
-					<Section
-						title="Accessories"
-						description=""
-						backgroundImg="accessories.jpg"
-						leftBtnText="Shop now"
-					/>
-				</>
-			)}
+			<Section
+				title="Accessories"
+				description=""
+				backgroundImg="accessories.jpg"
+				leftBtnText="Shop now"
+			/>
 		</Container>
 	)
 }
